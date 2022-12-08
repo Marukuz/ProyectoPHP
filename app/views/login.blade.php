5fc95be47e9433e21c55279e933586dd2e00c43f
Marc Garrido <marccocmc@gmail.com>
2022-12-09 00:00:50 +0100

    Avance Proyecto

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -17,21 +17,16 @@
                     <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                         <div class="card shadow-2-strong" style="border-radius: 1rem;">
                             <div class="card-body p-5 text-center">
-
                                 <h3 class="mb-5">Login</h3>
-
                                 <div class="form-outline mb-4">
                                     <label class="form-label" for="typeEmailX-2">Usuario</label>
                                     <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
                                 </div>
-
                                 <div class="form-outline mb-4">
                                     <label class="form-label" for="typePasswordX-2">Contraseña</label>
                                     <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />                        
                                 </div>
-
                                 <input class="btn btn-primary btn-lg btn-block" type="submit">
-
                             </div>
                         </div>
                     </div>
