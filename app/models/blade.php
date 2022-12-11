b5c998aaf16c3ba0c4cb22bcdda8ac05be71ef1f
Marc Garrido <marccocmc@gmail.com>
2022-12-11 17:08:41 +0100

    Blade y conexion PDO documentados

--- app/models/blade.php
+++ app/models/blade.php
@@ -2,9 +2,12 @@
 
 use Jenssegers\Blade\Blade;
 
+// Definimos las rutas VIEW y CACHE.
 define('VIEW_PATH', __DIR__.'/../views');
 define('CACHE_PATH', VIEW_PATH.'/cache');
+// Incluimos el autoload
 include (__DIR__.'/../../vendor/autoload.php');
 
+// Instanciamos blade.
 $blade = new Blade(VIEW_PATH, CACHE_PATH);
 
--- app/models/conexionPDO.php
+++ app/models/conexionPDO.php
@@ -1,13 +1,30 @@
 <?php 
 
+/**
+ * Conexion
+ * 
+ * Clase para instanciar la conexion con el metodo singleton a la base de datos mediante PDO.
+ */
 class Conexion{
+    
     public $base;
     private static $instance;
-
+    
+    /**
+     * __construct
+     *
+     * Instanciamos una nueva conexion mediante PDO.
+     * 
+     */
     public function __construct (){
         $this->base = new PDO('mysql:host=localhost;dbname=proyecto','root','');
     }
-    
+        
+    /**
+     * getInstance
+     *
+     * Llamamos a la instancia y si no existe la creamos seguidamente para retornarla.
+     */
     public static function getInstance(){
         if(!isset(self::$instance)){
             $conex = __CLASS__;
