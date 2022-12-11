a9b36deff07345a2836539d6e5ad040e5c8383e8
Marc Garrido <marccocmc@gmail.com>
2022-12-11 18:54:49 +0100

    PROYECTO FINAL V1

--- app/views/añadirUsuario.blade.php
+++ app/views/añadirUsuario.blade.php
@@ -4,12 +4,15 @@
     <h1 style="text-align: center;">Añadir Usuario</h1><br>
     <div id="formulario" >
         <form method="post">
+            <!-- Filtrado de errores -->
             Usuario: {!!$error->ErrorFormateado("nombre")!!} <br> 
             <input type="text" class="form-control" name="usuario" value="{{$usuario[0] ?? ''}}"><br>
-
+        
+            <!-- Filtrado de errores -->
             Contraseña: {!!$error->ErrorFormateado("contraseña")!!}<br>
             <input type="password" class="form-control" name="password" value="{{$usuario[1] ?? ''}}"><br>
 
+            <!-- Filtrado de errores -->
             Rol: {!!$error->ErrorFormateado("rol")!!}<br>
             <select class="form-select" name="rol">
                 <option disabled selected>Selecciona</option>
--- app/views/modificarTarea.blade.php
+++ app/views/modificarTarea.blade.php
@@ -6,32 +6,32 @@
             ID Tarea: <br>
             <input type="text" class="form-control" name="id" value="{{$tarea['tarea_id'] ?? ''}}" readonly><br>
             
-            <!-- Mostramos con blade los errores -->
+            <!-- Filtrado de errores -->
             DNI: {!!$error->ErrorFormateado("dni")!!} <br>
             <input type="text" class="form-control" name="dni" value="{{$tarea['dni'] ?? ''}}"><br>
 
-            
+            <!-- Filtrado de errores -->
             Nombre:  {!!$error->ErrorFormateado("nombre")!!}<br>
             <input type="text" class="form-control" name="nombre" value="{{$tarea['nombre'] ?? ''}}"><br>
-
+            <!-- Filtrado de errores -->
             Apellido: {!!$error->ErrorFormateado("apellido")!!}<br>
             <input type="text" class="form-control" name="apellido" value="{{$tarea['apellido'] ?? ''}}"><br>
-
+            <!-- Filtrado de errores -->
             Telefono: {!!$error->ErrorFormateado("telefono")!!}<br>
             <input type="text" class="form-control" name="telefono" value="{{$tarea['telefono'] ?? ''}}"><br>
-
+            <!-- Filtrado de errores -->
             Correo Electronico: {!!$error->ErrorFormateado("correo")!!}<br>
             <input type="text" class="form-control" name="correo" value="{{$tarea['correo'] ?? ''}}"><br>
-
+            <!-- Filtrado de errores -->
             Direccion: {!!$error->ErrorFormateado("direccion")!!}<br>
             <input type="text" class="form-control" name="direccion" value="{{$tarea['direccion'] ?? ''}}"><br>
-
+            <!-- Filtrado de errores -->
             Poblacion: {!!$error->ErrorFormateado("poblacion")!!}<br>
             <input type="text" class="form-control" name="poblacion" value="{{$tarea['poblacion'] ?? ''}}"><br>
-
+            <!-- Filtrado de errores -->
             Codigo Postal: {!!$error->ErrorFormateado("codigop")!!}<br>
             <input type="text" class="form-control" name="codigop" value="{{$tarea['codigo_postal'] ?? ''}}"><br>
-
+            <!-- Filtrado de errores -->
             Provincia: {!!$error->ErrorFormateado("provincia")!!} <br>
             <select class="form-select" name="provincia">
                 <option selected>{{$tarea['provincia']}}</option>
@@ -39,7 +39,7 @@
                 <option>{{$provincia['nombre']}}</option>
                 @endforeach
             </select><br>
-
+            <!-- Filtrado de errores -->
             Estado Tarea: {!!$error->ErrorFormateado("estado")!!}<br>
             <select class="form-select" name="estado">
                 <option selected>{{$tarea['estado_tarea']}}</option>
@@ -51,7 +51,7 @@
 
             Fecha creacion: <br>
             <input type="date" class="form-control" name="fechac" value="{{$tarea['fecha_creacion'] ?? ''}}" readonly><br>
-
+            <!-- Filtrado de errores -->
             Operario encargado: {!!$error->ErrorFormateado("operario")!!} <br>
             <select class="form-select" name="operario">
                 <option selected>{{$tarea['operario_encargado']}}</option>
@@ -61,13 +61,13 @@
                 <option>Jesus</option>
                 <option>David</option>
             </select><br>
-
+            <!-- Filtrado de errores -->
             Fecha Realizacion: {!!$error->ErrorFormateado("fecha")!!}<br>
             <input type="date" class="form-control" name="fecha"  value="{{$tarea['fecha_realizacion'] ?? ''}}"><br>
-
+            <!-- Filtrado de errores -->
             Descripcion: {!!$error->ErrorFormateado("descripcion")!!}<br>
             <textarea class="form-control" name="descripcion">{{$tarea['descripcion']}}</textarea><br>
-
+            <!-- Filtrado de errores -->
             Anotacion inicial: {!!$error->ErrorFormateado("anotacioni")!!}<br>
             <textarea class="form-control" name="anotacioni" >{{$tarea['anotacion_inicio'] }}</textarea><br>
 
