c95dbfc2ac8a7efbdefdfe0153c9ad6bac8712d8
Marc Garrido <marccocmc@gmail.com>
2022-12-11 16:50:32 +0100

    Cambios varios

--- app/bladeejemplos/menu.blade.php
+++
@@ -1,4 +0,0 @@
-<div style="float:left; width:8em; border-radius: 5px; border:1px solid #aaa; background: #ccffcc; margin-right: 1em;">
-    <a href="../controllers/procesarlistaTareas.php">Ver Lista Tareas</a>
-    <a href="../controllers/procesarlistaTareasPendientes.php">Ver Tareas Pendientes</a>
-</div>
